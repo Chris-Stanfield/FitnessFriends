8ada0fcd4233fb461c55fd91b541edbad21e01b1
Steven-Katona <89264529+Steven-Katona@users.noreply.github.com>
2021-09-20 17:58:26 -0400

    added modules

--- fitnessfriends/src/app/app.module.ts
+++ fitnessfriends/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelpPageComponent } from './help-page/help-page.component';
+import { HelppageComponent } from './helppage/helppage.component';
+import { ProfileComponent } from './profile/profile.component';
+import { OrganizationComponent } from './organization/organization.component';
+import { GpsComponent } from './gps/gps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelpPageComponent,
+    HelppageComponent,
+    ProfileComponent,
+    OrganizationComponent,
+    GpsComponent
   ],
   imports: [
     BrowserModule,
