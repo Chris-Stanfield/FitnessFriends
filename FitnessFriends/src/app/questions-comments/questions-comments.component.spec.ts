953c4b4b269738af0d6a51ca4b0d53df75572c99
Steven-Katona <89264529+Steven-Katona@users.noreply.github.com>
2021-09-30 12:44:06 -0400

    added post and question skeleton

---
+++ FitnessFriends/src/app/questions-comments/questions-comments.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QuestionsCommentsComponent } from './questions-comments.component';
+
+describe('QuestionsCommentsComponent', () => {
+  let component: QuestionsCommentsComponent;
+  let fixture: ComponentFixture<QuestionsCommentsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ QuestionsCommentsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QuestionsCommentsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ FitnessFriends/src/app/questions-comments/questions-comments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-questions-comments',
+  templateUrl: './questions-comments.component.html',
+  styleUrls: ['./questions-comments.component.css']
+})
+export class QuestionsCommentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
