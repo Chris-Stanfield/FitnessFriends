e8875a8c4642ecd4e82b5eaab9d26ade668b682c
Steven-Katona <89264529+Steven-Katona@users.noreply.github.com>
2021-09-30 13:04:37 -0400

    html file is now real in post

--- FitnessFriends/src/app/app.module.ts
+++ FitnessFriends/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginSignupComponent } from './login-signup/login-signup.component';
+import { PostComponent } from './post/post.component';
+import { QuestionsCommentsComponent } from './questions-comments/questions-comments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginSignupComponent
+    LoginSignupComponent,
+    PostComponent,
+    QuestionsCommentsComponent
   ],
   imports: [
     BrowserModule,
