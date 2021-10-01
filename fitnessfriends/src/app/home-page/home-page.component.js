a23a3fb062593cb54d4b099bbff8441a42036d16
cms5377 <79059899+cms5377@users.noreply.github.com>
2021-09-30 23:12:14 -0400

    added hello_world function to home-page and got rid of local homepage

---
+++ fitnessfriends/src/app/home-page/home-page.component.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.hello_world = void 0;
+function hello_world() {
+    console.log("Hello World");
+    return "hello World";
+}
+exports.hello_world = hello_world;
+hello_world();
--- fitnessfriends/src/app/home-page/home-page.component.ts
+++ fitnessfriends/src/app/home-page/home-page.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
-})
-export class HomePageComponent implements OnInit {
+export function hello_world() {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    console.log("Hello World")
+    return "hello World";
+  
 }
+
+hello_world();
