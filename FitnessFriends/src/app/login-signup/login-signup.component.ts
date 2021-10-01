9201035604e0230ab5c883f3c39afec22429d12b
Nicklaus <nar4026@uncw.edu>
2021-09-30 21:57:33 -0400

    Fixed the Login-signup module to properly show hello world in the console. Added local server files to the login-signup component folder for testing.

--- FitnessFriends/src/app/login-signup/login-signup.component.ts
+++ FitnessFriends/src/app/login-signup/login-signup.component.ts
@@ -1,19 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-login-signup',
-  templateUrl: './login-signup.component.html',
-  styleUrls: ['./login-signup.component.css']
-})
-export class LoginSignupComponent implements OnInit {
-
-  constructor() { }
-  hello_world() {
+  export function hello_world() {
     console.log("Hello world!");
   }
 
-  ngOnInit(): void {
-    this.hello_world();
-  }
-
-}
+  hello_world()
