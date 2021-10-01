450dd578e4ea7c40d428c8c67f6e01a82d494339
Steven-Katona <89264529+Steven-Katona@users.noreply.github.com>
2021-10-01 11:30:37 -0400

    final main push

--- FitnessFriends/src/app/app.module.ts
+++ FitnessFriends/src/app/app.module.ts
@@ -4,21 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginSignupComponent } from './login-signup/login-signup.component';
-import { PostComponent } from './post/post.component';
-import { QuestionsCommentsComponent } from './questions-comments/questions-comments.component';
-import { FeedbackComponent } from './feedback/feedback.component';
-import { RecentComponent } from './recent/recent.component';
-import { FeedComponent } from './feed/feed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginSignupComponent,
-    PostComponent,
-    QuestionsCommentsComponent,
-    FeedbackComponent,
-    RecentComponent,
-    FeedComponent
+    LoginSignupComponent
   ],
   imports: [
     BrowserModule,
