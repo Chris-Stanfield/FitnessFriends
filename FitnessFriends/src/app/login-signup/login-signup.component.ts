e8415697186741640facc88980bbbc23d37cc348
Nick R <nar4026@uncw.edu>
2021-10-15 12:28:25 -0400

    Created .js backend for Login page and updated paramaters in the html file

--- FitnessFriends/src/app/login-signup/login-signup.component.ts
+++ FitnessFriends/src/app/login-signup/login-signup.component.ts
@@ -1,9 +1,23 @@
+const form: HTMLFormElement = document.querySelector('login') as HTMLFormElement;
+
+form.onsubmit = () => {
+  const formData = new FormData(form);
+
+  const userName = formData.get('userName') as string;
+  
+  const pass = formData.get('pass') as string;
+
+  if (auth(userName, pass)){
+    /*Route to login page*/
+  }
+  else{
+    /*Return error*/
+  }
 
 
-export function hello_world() {
-  console.log("Hello world!");
-  return "Hello from login-signup!";
+  return false;
 }
+
 
 
 /**
@@ -12,8 +26,14 @@ export function hello_world() {
 *
 *@param {string} target - The username or authentication token of the user you want to authenticate
 *
+*@param {string} id - The unique identifier that goes with the user you want to authenticate
+*
 *return: A boolean indicating if the user exists in the system or not.
 */
-export function auth(target: string) {
+export function auth(target: string, id: string) {
+  let userName = target
+  let pass = id
+
+
   return false;
 }
