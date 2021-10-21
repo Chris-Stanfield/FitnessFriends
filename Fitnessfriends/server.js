0f90e4b7043285a5f8b586d69ccda71929ae2a73
cms5377 <79059899+cms5377@users.noreply.github.com>
2021-10-20 21:17:00 -0400

    lower case f for friends. lol

--- Fitnessfriends/server.js
+++ Fitnessfriends/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/FitnessFriends'));
+app.use(express.static('./dist/Fitnessfriends'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/FitnessFriends/' }),
+    res.sendFile('index.html', { root: 'dist/Fitnessfriends/' }),
 );
 
 // Start the app by listening on the default Heroku port
