121851b36f7b6c94101901205f799c57cd1ad662
cms5377 <79059899+cms5377@users.noreply.github.com>
2021-10-20 21:10:15 -0400

    keep getting rejected when pushing heroku to main

--- Fitnessfriends/server.js
+++ Fitnessfriends/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/global-fitness'));
+app.use(express.static('./dist/software-engineering-team-project-fourtothefloor'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/global-fitness/' }),
+    res.sendFile('index.html', { root: 'dist/software-engineering-team-project-fourtothefloor/' }),
 );
 
 // Start the app by listening on the default Heroku port
