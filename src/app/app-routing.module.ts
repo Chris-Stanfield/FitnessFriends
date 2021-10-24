67d8167511bebdbcee312cb0ca52c19b7a350bef
Lucas Layman <laymanl@uncw.edu>
2021-10-23 22:16:00 -0400

    Added a very basic 'launch' page that links create, view, and home. Login/signup is not included yet.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ViewPostComponent } from './view-post/view-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'createpost', component: CreatePostComponent},
+  { path: 'home', component: HomePageComponent},
+  { path: 'viewpost', component: ViewPostComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
