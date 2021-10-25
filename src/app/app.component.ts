aa59607fa74aba52dc505148f01a2ece0b30452a
Steven-Katona <89264529+Steven-Katona@users.noreply.github.com>
2021-10-25 16:59:03 -0400

    edited the create-post code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './create-post/create-post.component.html',
+  styleUrls: ['./create-post/create-post.component.css']
 })
 export class AppComponent {
   title = 'Fitnessfriends';
