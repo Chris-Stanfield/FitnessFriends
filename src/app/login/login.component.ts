7a6d796ad4fcc60b93cd41a1cdeae12f2b0c9035
Nicklaus <nar4026@uncw.edu>
2021-10-26 11:32:26 -0400

    Updated branch with changes to main, replaced login-signup with login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  const form: HTMLFormElement = document.querySelector('login') as HTMLFormElement;
+
+  form.onsubmit = () => {
+    const formData = new FormData(form);
+
+    const userName = formData.get('userName') as string;
+  
+    const pass = formData.get('pass') as string;
+
+    if (auth(userName, pass)){
+      /*Route to login page*/
+    }
+    else{
+      /*Return error*/
+    }
+
+
+    return false;
+  }
+}
+
+/**
+*
+*auth takes a string as input and ensures the user exists in the database
+*
+*@param {string} target - The username or authentication token of the user you want to authenticate
+*
+*@param {string} id - The unique identifier that goes with the user you want to authenticate
+*
+*return: A boolean indicating if the user exists in the system or not.
+*/
+export function auth(target: string, id: string) {
+  let userName = target
+  let pass = id
+
+
+  return false;
+}
