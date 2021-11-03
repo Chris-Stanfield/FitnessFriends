8e476e1e0aa0c2b59d640487a955a3737305d7f3
cms5377 <79059899+cms5377@users.noreply.github.com>
2021-11-03 16:39:27 -0400

    updated server file

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/software-engineering-team-project-fourtothefloor'));
+app.use(express.static('./dist/Fitnessfriends'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/software-engineering-team-project-fourtothefloor/' }),
+    res.sendFile('index.html', { root: 'dist/Fitnessfriends/' }),
 );
 
 // Start the app by listening on the default Heroku port
