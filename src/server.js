732a70b62c7e5dff927da16fda23d8bda2b7906e
Steven-Katona <89264529+Steven-Katona@users.noreply.github.com>
2021-11-04 19:39:10 -0400

    new settup for maybe talking to a database

---
+++ src/server.js
@@ -0,0 +1,23 @@
+//Install express server
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const path = require('path');
+const app = express();
+const db = require("./app/models");
+
+
+
+var corsOptions = {origin: "http://localhost:8081"};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+db.sequelize.sync();
+
+app.get('/*', (req, res) => { res.sendFile('index.html', { root: 'dist/software-engineering-team-project-fourtothefloor/' }) });
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {console.log(`server is on ${ PORT }.`)});
