15432aeb0d7c73d09ae15428e24de7271d86d2ea
Chris <cms5377@uncw.edu>
2021-12-01 16:24:13 -0500

    found out I had have pictures in the assests folder to load images. Angular sucks

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
   selectedFile = null;
- 
+  chrisImage:string = "../src/app/home-page/pictures/pic1.jpeg"
+
   onFileSelected (event:any) {
     this.selectedFile = event.target.files[0];
   }
