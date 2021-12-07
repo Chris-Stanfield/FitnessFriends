e7213c23505dcb1a66fc0e364cdbcfd75e83bb5d
Chris <cms5377@uncw.edu>
2021-12-06 21:59:55 -0500

    submit button works

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-feedback',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackComponent implements OnInit {
 
-  constructor() { }
+  
+  constructor(private router: Router) { }
+  
+  
+  onSubmit(){
+    this.router.navigateByUrl("/home");
+  }
 
   ngOnInit(): void {
   }
